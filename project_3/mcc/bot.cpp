76b225cbef951792236c2a25a99e92819be63de3
Marin Pavlinov Marinov <33673401+marinov98@users.noreply.github.com>
2018-04-15 11:50:21 -0400

    Add files via upload

--- project_3/mcc/bot.cpp
+++ project_3/mcc/bot.cpp
@@ -1,3 +1,7 @@
+//Author: Marin Pavlinov Marinov
+//Course: Csci-135
+//Instructor: Genady Maryashi
+//Assignment: project 3
 
 #include <cstdlib>
 #include <iostream>
@@ -10,6 +14,12 @@ const int MAX_ROBOT_NUM = 50;
 int NUM;          // to remember number or robots
 int ROWS, COLS;   // map dimensions
 
+// int row_Destination(MAX_ROBOT_NUM)={};
+//
+// int col_Destination(MAX_ROBOT_NUM)={};
+// bool robot_needs_location[MAX_ROBOT_NUM]={false};
+
+
 /* Initialization procedure, called when the game starts: */
 void onStart(int num, int rows, int cols, double mpr,Area &area, ostream &log)
 {
@@ -20,15 +30,15 @@ void onStart(int num, int rows, int cols, double mpr,Area &area, ostream &log)
 	log << "Start!" << endl;
 }
 
-
-
 /* Deciding robot's next move */
-Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
+Action onRobotAction(int id, Loc loc, Area &area, ostream &log)
+{
 	int row = loc.r; // current row and column
 	int col = loc.c;
 
 	if (area.inspect(row, col) == DEBRIS)
 		return COLLECT;
+	//if the debris is either one square away, go to it
   else if (area.inspect(row+1,col)==DEBRIS)
   {
     return DOWN;
@@ -45,19 +55,95 @@ Action onRobotAction(int id, Loc loc, Area &area, ostream &log) {
   {
     return LEFT;
   }
+	//next set	of statements check for DEBRIS that is farther away
 	else
   {
-		// if not at a debris field, move randomly:
+		////if its just one column and row away, zig zag towards it
+	 if (area.inspect(row+1,col+1)==DEBRIS)
+		{
+			switch (rand() % 2)
+			{
+				case 0:
+					return DOWN;
+				case 1:
+					return RIGHT;
+			}
+		}
+		else if (area.inspect(row+1,col-1)==DEBRIS)
+		{
+			switch (rand() % 2)
+			{
+				case 0:
+					return DOWN;
+				case 1:
+					return LEFT;
+			}
+		}
+		else if (area.inspect(row-1,col+1)==DEBRIS)
+		{
+			switch (rand() % 2)
+			{
+				case 0:
+					return UP;
+				case 1:
+					return RIGHT;
+			}
+		}
+		else if (area.inspect(row-1,col-1)==DEBRIS)
+		{
+			switch (rand() % 2)
+			{
+				case 0:
+					return UP;
+				case 1:
+					return LEFT;
+			}
+		}
+		//////////////////////////////////////////////////////////
+		//when a Debris is near either horizontally or vertically, go straight towards it
+		for (int j=COLS;j>1;j--)//these for loops check how many columns  and rows the robot is away from the debris
+		{
+					//each if statement corresponds to the proper way the robot should zig zag towards the debris
+			if (area.inspect(row,col+j)==DEBRIS)
+			{
+					return RIGHT;
+			}
+		}
+		///////////////////////////////////
+		for (int k=COLS;k>1;k--)
+		{
+			if(area.inspect(row,col-k)==DEBRIS)
+			{
+					return LEFT;
+			}
+		}
+		///////////////////////////////
+		for (int i=ROWS;i>1;i--)
+		{
+			if(area.inspect(row+i,col)==DEBRIS)
+			{
+				return DOWN;
+			}
+		}
+		///////////////////////////////
+		for (int x=ROWS;x>1;x--)
+		{
+			if(area.inspect(row-x,col)==DEBRIS)
+			{
+				return UP;
+			}
+		}
+		//if not at a debris field, move randomly:
 		switch(rand() % 4)
     {
-		case 0:
-			return LEFT;
-		case 1:
-			return RIGHT;
-		case 2:
-			return UP;
-		default:
-			return DOWN;
+			case 0:
+				return LEFT;
+			case 1:
+				return RIGHT;
+			case 2:
+				return UP;
+			default:
+				return DOWN;
 		}
 	}
 }
